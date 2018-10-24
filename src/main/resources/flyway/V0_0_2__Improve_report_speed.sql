bd4f32007491be040f01bdb2bfa1b189a2027c67
jakubcaj <jakubcaj@gmail.com>
2018-10-24 21:25:56 +0200

    Alter employee matches report to improve speed 3

--- src/main/resources/flyway/V0_0_2__Improve_report_speed.sql
+++ src/main/resources/flyway/V0_0_2__Improve_report_speed.sql
@@ -1,3 +1,22 @@
+create or replace view helper.match_winners as
+  (select
+          subQ.match_id,
+          subQ.employee_id,
+          subQ.total_goals as total_goal_count
+   from (select subQ.match_id,
+                subQ.employee_id,
+                subQ.total_goals,
+                row_number() over (partition by match_id order by win_count desc) as row_number
+         from (select m.id                as match_id,
+                      rw.employee_id,
+                      count(*)            as win_count,
+                      sum(rw.goal_count) as total_goals
+               from helper.round_winners rw
+                      join round r on r.id = rw.round_id
+                      join match m on r.match_id = m.id and m.finished = true
+               group by m.id, rw.employee_id) subQ) subQ
+   where subQ.row_number in (1, 2));
+
 create or replace view report.employee_matches_report as
   (select query.id,
           query.employee_id,
